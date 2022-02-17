f61c23a0ca79a99549ebbc40acb1af17e1d76e6c
sourav1547 <souravd2@illinois.edu>
2022-02-17 13:12:06 -0600

    i/o interface comment added

--- bls/bls_ths.py
+++ bls/bls_ths.py
@@ -2,7 +2,7 @@ from utils import Fp, random_fp, bls12_381, G1, sha256, hash_to_G2
 
 # TODO: To generate a secret key shares of each node and the threshold public keys of every node
 # INPUT:: n: total number of nodes; t: fault threshold
-# OUTPUT:: [threshold secret keys], [threshold public keys]
+# OUTPUT:: bls public key; [threshold secret keys]; [threshold public keys]
 # NOTE: Although this function is also returning a list of private keys, in practice the key generation
 # protocol will only return the i-th secret share to i-th node.  
 def generate_bls_ths_keys(n: int, t: int) -> tuple[bls12_381.G1, list[Fp], list[bls12_381.G1]]:
