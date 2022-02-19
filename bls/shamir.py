f551c6190018b6a347bec1fa5d155c0bc06e0425
sourav1547 <souravd2@illinois.edu>
2022-02-19 03:40:42 -0600

    interface changes

--- bls/shamir.py
+++ bls/shamir.py
@@ -5,34 +5,12 @@ from utils import Fp, bls12_381
 
 # TODO: return a vector of shares [s1, s2, ..., sn]
 # INPUT:: n: number of nodes, t: threshold, secret, and a field
-# OUTPUT:: Shamir secret shares [s1, s2, ..., sn] where each si is a field element
-def gen_share(n: int, t: int, secret: Fp, field=Fp) -> list[Fp]:
+# OUTPUT:: Shamir secret shares [(1, s1), (2,s2), ..., (n,sn)] where each si is a field element
+def gen_share(n: int, t: int, secret: Fp, field=Fp) -> list[(int, Fp)]:
     return None
 
 # TODO: interpolate and return the point at 0
-# INPUT:: shares: a list of tuples [(1,a), (2,b), (4,c)]
+# INPUT:: shares: a list of tuples [(x1,a), (x2,b),...]
 # OUTPUT:: P(0): here P(x) is the polynomial defined by the shares
 def interpolate_at_0(shares : list[(int, Fp)]) -> Fp:
-    return None
-
-# TODO: interpolate and return the point at an j
-# INPUT:: shares: a list of tuples [(1,a), (2,b), (4,c)] and an index j
-# OUTPUT:: P(j): the polynomial defined by the shares
-def interpolate_at_j(shares: list[(int, Fp)], j: int) -> Fp:
-    return None
-
-# TODO: Interpolate in the exponent at 0, i.e., compute G1^{P(0)} 
-# where P(x) is the polynomial defined by the shares
-# INPUT:: shares: a list of tuples [(1,G1^a), (2,G1^b), (4,G1^c)] and index j
-# OUTPUT:: G1^P(0): here P(x) is the polynomial defined by the shares
-# NOTE: You need to work with `Fp = FiniteField(bls12_381.curve_order, 1)` for this function
-def interpolate_at_g0(shares: list[(int, bls12_381.G1)]) -> bls12_381.G1:
-    return None
-
-# TODO: Interpolate in the exponent at an index j, i.e., compute G1^{P(j)} 
-# where P(x) is the polynomial defined by the shares
-# INPUT:: shares: a list of tuples [(1,G1^a), (2,G1^b), (4,G1^c)] and index j
-# OUTPUT:: G1^P(j): here P(x) is the polynomial defined by the shares
-# NOTE: You need to work with `Fp = FiniteField(bls12_381.curve_order, 1)` for this function
-def interpolate_at_gj(shares: list[(int, bls12_381.G1)], j: int) -> bls12_381.G1:
     return None
